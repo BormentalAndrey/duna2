972baf2d452890edcd6e36b212e58144ca057cdb
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-02 00:07:02 +0300

    Update flac_fixes.h

--- app/src/main/cpp/flac_fixes.h
+++ app/src/main/cpp/flac_fixes.h
@@ -1,7 +1,20 @@
 #ifndef FLAC_FIXES_H
 #define FLAC_FIXES_H
 
-// Принудительное определение макросов минимума/максимума для старого кода libFLAC
+// 1. Фикс для format.c: задаем версию библиотеки FLAC
+#ifndef PACKAGE_VERSION
+#define PACKAGE_VERSION "1.3.2"
+#endif
+
+// 2. Фикс для lpc.c: сообщаем FLAC, что в Android уже есть системный lround
+#ifndef HAVE_LROUND
+#define HAVE_LROUND 1
+#endif
+#ifndef HAVE_LROUNDF
+#define HAVE_LROUNDF 1
+#endif
+
+// 3. Предыдущие фиксы для вычисления минимума/максимума
 #ifndef flac_min
 #define flac_min(a,b) ((a)<(b)?(a):(b))
 #endif
