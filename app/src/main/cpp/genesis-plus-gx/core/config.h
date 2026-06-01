0bcbcd1b35533b035fdefb4da74492d62e783930
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-01 22:37:12 +0300

    Create config.h

---
+++ app/src/main/cpp/genesis-plus-gx/core/config.h
@@ -0,0 +1,10 @@
+cat > app/src/main/cpp/genesis-plus-gx/core/config.h << 'EOF'
+#ifndef CONFIG_H
+#define CONFIG_H
+#define HAVE_INTTYPES 1
+#define HAVE_STDINT 1
+#define LSB_FIRST 1
+#define ALIGN_LONG 1
+#define USE_16BPP_RENDERING 1
+#endif
+EOF
