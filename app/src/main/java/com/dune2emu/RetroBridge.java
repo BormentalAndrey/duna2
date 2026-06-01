b517da209f1c124782b95a31230dbd4ba88402d2
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-01 20:30:01 +0300

    Create RetroBridge.java

---
+++ app/src/main/java/com/dune2emu/RetroBridge.java
@@ -0,0 +1,15 @@
+package com.dune2emu;
+
+public class RetroBridge {
+    static {
+        System.loadLibrary("dune2emu");
+    }
+    
+    public native boolean initEmulator(String romPath, String saveDir);
+    public native boolean setSurface(Object surface);
+    public native void startEmulation();
+    public native void stopEmulation();
+    public native void setButtonState(int player, int button, boolean pressed);
+    public native boolean saveState(int slot);
+    public native boolean loadState(int slot);
+}
